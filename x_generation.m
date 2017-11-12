f11f9e20da1dec4fd152c7cdacd5b6a6820e79d9
marv3 <joao.lizarraga@gmail.com>
2017-11-12 16:39:41 -0500

    Changing comments from spanish to english

--- x_generation.m
+++ x_generation.m
@@ -3,26 +3,26 @@ dt= 0.0001;
 
 t0= dt;
 
-% Condiciones iniciales
+% Initial conditions
 x0= -0.3;
 xd0= 0.8;
 xf= -dt;
 
-tau= x0/ xd0; % tau< 0 (Para que x(t) converja a 0).
-lambda= x0/ exp(t0/ tau); % lambda<0 (para que tf sea real).
+tau= x0/ xd0; % tau< 0 (Condition for "x(t)" to converge to "0" ).
+lambda= x0/ exp(t0/ tau); % lambda<0 (Condition for "tf" to be real).
 
 tf= tau* log(xf/ lambda);
 
-t= t0: dt: tf; % Vector de tiempo
-x_t= lambda* exp(t/ tau); % Posici?n x(t)
-    xd_t= lambda/ tau* exp(t/ tau); % Velocidad
+t= t0: dt: tf; % Time vector
+x_t= lambda* exp(t/ tau); % Position x(t)
+    xd_t= lambda/ tau* exp(t/ tau); % Velocity
 
-% Descripci?n gr?fica:
+% Graphic description
 figure
-plot(t, x_t), title('Posici?n generada x(t)');
-xlabel('tiempo'), ylabel('posici?n')
+plot(t, x_t), title('Position x(t)');
+xlabel('Time'), ylabel('Position')
 
 figure
-plot(t, xd_t), title('Velocidad en x(t)');
-xlabel('tiempo'), ylabel('velocidad')
+plot(t, xd_t), title('Velocity of x(t)');
+xlabel('Time'), ylabel('Velocity')
  
